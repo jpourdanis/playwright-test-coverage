443ff2dcbf3603c073f7d0b35e9d3d75201dcc1f
“pourdanisj” <pourdanisj@vmware.com>
2025-05-16 11:20:42 +0300

    added typed color object for assertion

--- e2e/App.test.ts
+++ e2e/App.test.ts
@@ -5,6 +5,15 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
+interface Color {
+  name: string;
+  hex: string;
+}
+
+let TurquoiseColor: Color = { name: "Turquoise", hex: "1abc9c" };
+let RedColor: Color = { name: "Red", hex: "e74c3c" };
+let YellowColor: Color = { name: "Yellow", hex: "f1c40f" };
+
 test("check Turquoise ( #1abc9c) is the default background color.", async ({
   page,
 }) => {
@@ -12,6 +21,7 @@ test("check Turquoise ( #1abc9c) is the default background color.", async ({
     .locator("text=Current color:")
     .textContent();
   let hex = extractHexColor(currentColorText);
+  expect(hex).toBe(TurquoiseColor.hex);
   let rgbColors = convertHexToRGB(`#${hex}`);
   await expect(page.locator("header")).toHaveCSS(
     "background-color",
@@ -26,6 +36,7 @@ test("check Turquoise ( #1abc9c) is the default background color.", async ({
 //     .locator("text=Current color:")
 //     .textContent();
 //   let hex = extractHexColor(currentColorText);
+//   expect(hex).toBe(RedColor.hex);
 //   let rgbColors = convertHexToRGB(`#${hex}`);
 //   await expect(page.locator("header")).toHaveCSS(
 //     "background-color",
@@ -40,6 +51,7 @@ test("check Turquoise ( #1abc9c) is the default background color.", async ({
 //     .locator("text=Current color:")
 //     .textContent();
 //   let hex = extractHexColor(currentColorText);
+//   expect(hex).toBe(YellowColor.hex);
 //   let rgbColors = convertHexToRGB(`#${hex}`);
 //   await expect(page.locator("header")).toHaveCSS(
 //     "background-color",
