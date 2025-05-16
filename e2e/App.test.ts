ddb2673c07338613fdc0cb7cd99d2574558b6905
“pourdanisj” <pourdanisj@vmware.com>
2025-05-16 10:44:12 +0300

    refactor with css checking on rgb

--- e2e/App.test.ts
+++ e2e/App.test.ts
@@ -1,19 +1,48 @@
-import { test } from "./baseFixtures";
+import { test, expect } from "./baseFixtures";
+import { convertHexToRGB, extractHexColor } from "./helper";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
-test("use Turquoise as a default background color", async ({ page }) => {
-  await page.waitForSelector("text=#1abc9c");
+test("check Turquoise ( #1abc9c) is the default background color.", async ({
+  page,
+}) => {
+  let currentColorText = await page
+    .locator("text=Current color:")
+    .textContent();
+  let hex = extractHexColor(currentColorText);
+  let rgbColors = convertHexToRGB(`#${hex}`);
+  await expect(page.locator("header")).toHaveCSS(
+    "background-color",
+    `rgb(${rgbColors.red}, ${rgbColors.green}, ${rgbColors.blue})`
+  );
 });
 
-// test('use Red as a background color', async ({ page }) => {
-//   await page.click("text=Red")
-//   await page.waitForSelector("text=#e74c3c")
+// test("use Red ( #e74c3c) as a background color", async ({ page }) => {
+//   await page.click("text=Red");
+
+//   let currentColorText = await page
+//     .locator("text=Current color:")
+//     .textContent();
+//   let hex = extractHexColor(currentColorText);
+//   let rgbColors = convertHexToRGB(`#${hex}`);
+//   await expect(page.locator("header")).toHaveCSS(
+//     "background-color",
+//     `rgb(${rgbColors.red}, ${rgbColors.green}, ${rgbColors.blue})`
+//   );
 // });
 
-//test('use Yellow as a background color', async ({ page }) => {
-//  await page.click("text=Yellow")
-//  await page.waitForSelector("text=#f1c40f")
-//});
+// test("use Yellow ( #f1c40f) as a background color", async ({ page }) => {
+//   await page.click("text=Yellow");
+
+//   let currentColorText = await page
+//     .locator("text=Current color:")
+//     .textContent();
+//   let hex = extractHexColor(currentColorText);
+//   let rgbColors = convertHexToRGB(`#${hex}`);
+//   await expect(page.locator("header")).toHaveCSS(
+//     "background-color",
+//     `rgb(${rgbColors.red}, ${rgbColors.green}, ${rgbColors.blue})`
+//   );
+// });
---
+++ e2e/helper.ts
@@ -0,0 +1,24 @@
+export function convertHexToRGB(hex) {
+  // Remove the '#' if it's included in the input
+  hex = hex.replace(/^#/, "");
+
+  // Parse the hex values into separate R, G, and B values
+  const red = parseInt(hex.substring(0, 2), 16);
+  const green = parseInt(hex.substring(2, 4), 16);
+  const blue = parseInt(hex.substring(4, 6), 16);
+
+  // Return the RGB values in an object
+  return {
+    red: red,
+    green: green,
+    blue: blue,
+  };
+}
+
+export function extractHexColor(text) {
+  const hexMatch = text.match(/#([0-9a-fA-F]{6})/);
+  if (hexMatch) {
+    return hexMatch[1];
+  }
+  return null;
+}
