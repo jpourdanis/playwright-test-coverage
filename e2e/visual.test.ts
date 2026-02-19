267afd095e64112d680d4d49d572e439aecbd5df
“pourdanisj” <pourdanisj@vmware.com>
2026-02-19 14:41:41 +0200

    fix the failng test

--- e2e/visual.test.ts
+++ e2e/visual.test.ts
@@ -3,7 +3,6 @@ import { test, expect } from "./baseFixtures";
 test.describe("Visual regression", () => {
   test("homepage should match snapshot", async ({ page }) => {
     await page.goto("/");
-    await page.click("text=Red");
     await page.waitForSelector("header");
 
     const screenshot = await page.screenshot({ fullPage: true });
