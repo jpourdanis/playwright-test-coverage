5cd44aa45b260a1102e24b8433a71c0742bb2969
“pourdanisj” <pourdanisj@vmware.com>
2026-02-19 14:16:08 +0200

    make the test fail

--- e2e/visual.test.ts
+++ e2e/visual.test.ts
@@ -3,6 +3,7 @@ import { test, expect } from "./baseFixtures";
 test.describe("Visual regression", () => {
   test("homepage should match snapshot", async ({ page }) => {
     await page.goto("/");
+    await page.click("text=Red");
     await page.waitForSelector("header");
 
     const screenshot = await page.screenshot({ fullPage: true });
